ef498b1be53bfebae36da12238b22d915d173b87
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-07-02 20:53:27 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,43 @@
+
+        //  {
+        // // masala 1 faqat musbat sonlar qaytarilsin
+
+        // function numbers(array){
+        //     let sum = []
+        //     for(let i = 0, len = array.length; i < len; i++){
+        //      if(array[i] > 0){
+        //             sum.push(array[i])
+        //        }
+        //         }
+        //         return "musbat sonlar " + sum
+        //     }
+        //     console.log(numbers([-5, 6, 3, -3] ));
+        //     console.log(numbers([12, -4, 5, -6, 33] ));
+        // }
+    
+
+        // {
+        //     // masala 2 onliklar honasidagi sonni qaytaring
+
+        //     function tenth(number) {
+        //         return number / 10   
+        //     }
+
+
+        //     console.log(tenth(123)); 
+        //     console.log(tenth(4568)); 
+        // }
+
+
+        // {
+        //     // masala-7 eng katta va kichikini toping
+        //     function son(a, b, c, d, e){
+        //         if(a > b && b < c || a > b && a > c){
+        //             return `kichik: ${b}`
+        //         }else{
+        //             return `katta: ${a}`
+        //         }
+        //     }
+        //     console.log(son("1 2 3 4 5"));
+        //     console.log(son("5 25 33 44"));
+        // }
