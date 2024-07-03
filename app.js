527c09d6f4c3666e1ff4c399c51cc62c63aad71f
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-07-03 09:19:28 +0500

    jasa

--- app.js
+++ app.js
@@ -14,30 +14,85 @@
         //     console.log(numbers([-5, 6, 3, -3] ));
         //     console.log(numbers([12, -4, 5, -6, 33] ));
         // }
-    
 
+
+        
+        {
+            // 2-masala //
+            // function tenth(number){
+            // return Math.floor (number / 10 ) % 10
+            // }
+            // console.log(tenth(123)); // 2
+            // console.log(tenth(4568)); // 6
+        }
+        
+        {
+            // 3-masala //
+            // function binary(numbers){
+            //   return `${numbers === 1}` 
+            // }
+            // console.log( binary(1)); // true
+            // console.log(binary(0));
+        
+            //     function binary(number){
+            //     if(number === 1){
+            //         return true
+            //     }else if (number === 0){
+            //         return false
+            //     }else{
+            //         return null
+            //     }
+            // }
+            // console.log( binary(7)); // null
+        }
+        
+        {
+            // 4-masala //
+            // function card(cards, number) {
+            //     if (number) {
+            //         return cards;
+            //     } else {
+            //         let index = cards.slice(0, 4) + " **** **** **" + cards.slice(-2);
+            //         return index;
+            //     }
+            // }
+        
+            // console.log(card("8600 1234 5651 2589", true));  // 8600 1234 5651 2589
+            // console.log(card("8600 5464 2332 4589", false));  // 8600 **** **** **89
+        }
+        
+        {
+            // masala 5 //
+            //     function checkPassword(text){
+            //     return `${text === "12345hello"}`
+            // }
+            // console.log( checkPassword("12345678")); 
+            // console.log( checkPassword("12345hello")); 
+        }
+        
+        {
+            // masala 6 //
+            //     function camelCase(text) {
+            //     return text.split('-').map((son) => {
+            //         if (son === 0) return son.toLowerCase();
+            //         return son.charAt(0).toUpperCase() + son.slice(1).toLowerCase();
+            //     }).join('');
+            // }
+        
+            // console.log( camelCase("kabab-case") ); 
+            // console.log( camelCase("lorem-ipsum-dolor") ); 
+        }
+        
         // {
-        //     // masala 2 onliklar honasidagi sonni qaytaring
-
-        //     function tenth(number) {
-        //         return number / 10   
+        //       // masala 7 //
+        //         function bigAndSmall(text) {
+        //         let numbers = text.split(' ').map(Number);
+        //         let max = Math.max(...numbers);
+        //         let min = Math.min(...numbers);
+        //         return `${max} ${min}`;
         //     }
-
-
-        //     console.log(tenth(123)); 
-        //     console.log(tenth(4568)); 
+        
+        //     console.log(bigAndSmall("1 2 3 4 5")); 
+        //     console.log(bigAndSmall("5 25 33 44")); 
         // }
-
-
-        // {
-        //     // masala-7 eng katta va kichikini toping
-        //     function son(a, b, c, d, e){
-        //         if(a > b && b < c || a > b && a > c){
-        //             return `kichik: ${b}`
-        //         }else{
-        //             return `katta: ${a}`
-        //         }
-        //     }
-        //     console.log(son("1 2 3 4 5"));
-        //     console.log(son("5 25 33 44"));
-        // }
+          
